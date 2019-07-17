3935076489a66e1db37ef511846529367f3e25e4
BaharaJr <bennybenester@gmail.com>
2019-07-17 19:19:39 +0300

    :sparkles: Add the Navbar see #5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MainRoutes from "./feats/MainRoutes";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <div>
+        <Navbar />
         <MainRoutes />
       </div>
     </div>
