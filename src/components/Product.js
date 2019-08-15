b191b507ef9e9fffb660e291e7f90272a72f80f4
BaharaJr <bennybenester@gmail.com>
2019-08-15 01:31:33 -1200

    :sparkles: Add a single product view page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import defaultImg from '../images/details-112.jpg';
 
-class Product extends Component {
-    render() {
-        return (
-            <div className="loading">
-                <h4>Showing Products...</h4>
+function Product({product}) {
+    const{name, slug,images,price} = product;
+    return <article className="product"> 
+        <div className="img-container">
+            <img src={images[0] || defaultImg} alt="product" />
+            <div className="price-top">
+                <h6>Tshs{price}</h6>
+                <p>On Sale</p>
             </div>
-        );
+            <Link to={`/Gallery/${slug}`} className="btn-primary product-link">
+                View
+            </Link>
+        </div>
+        <p className="product-info">{name}</p>
+        </article>
     }
-}
+    Product.propTypes = {
+        product:PropTypes.shape({
+            name:PropTypes.string.isRequired, 
+            slug:PropTypes.string.isRequired, 
+            images:PropTypes.arrayOf(PropTypes.string).isRequired, 
+            price:PropTypes.number.isRequired, 
+        })
+    }
 
 export default Product;
