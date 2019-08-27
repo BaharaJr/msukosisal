c1383384c1a29dc9ce294029061cef452bfbbd03
BaharaJr <bennybenester@gmail.com>
2019-08-27 13:42:11 +0300

    :fire: Remove react icons from designs model

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import Title from "./Title";
-import { FaCocktail, FaHiking, FaShuttleVan } from "react-icons/fa";
+// import { FaCocktail, FaHiking, FaShuttleVan } from "react-icons/fa";
 export default class Services extends Component {
   state = {
     services: [
       {
-        icon: <FaCocktail />,
+        // icon: <FaCocktail />,
         title: "Scandinavian Twist",
         info:
           "A selection of items handmade by the Msuko Sisal women, made with 100% natural sisal fibre in the most sustainable way possible."
       },
       {
-        icon: <FaHiking />,
+        // icon: <FaHiking />,
         title: "Bohemian Style Decor",
         info:
           "Creating a Boho chic space in your house does not take a lot of effort, Adding a msuko sisal basket and plants also helps in capturing a sense of peace in the living room, bedroom or bathroom."
       },
       {
-        icon: <FaShuttleVan />,
+        // icon: <FaShuttleVan />,
         title: "Everything Sisal",
         info:
           "With Msuko Sisal, we aspire to inspire you to decorate your home with a touch of something that is sustainably made with sisal fiber."
@@ -32,7 +32,7 @@ export default class Services extends Component {
           {this.state.services.map((item, index) => {
             return (
               <article key={index} className="service">
-                <span>{item.icon}</span>
+                {/* <span>{item.icon}</span> */}
                 <h6>{item.title}</h6>
                 <p>{item.info}</p>
               </article>
