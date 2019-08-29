05b0946ccf36303331057f9e7838f18c923dbc3a
BaharaJr <bennybenester@gmail.com>
2019-08-29 12:41:05 +0300

    :fire: Add the filter by price option

--- src/components/ProductFilter.js
+++ src/components/ProductFilter.js
@@ -1,16 +1,67 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { ProductContext } from "../Context";
+import Title from "../components/Title";
 
-function ProductFilter() {
+//filter values
 
-return (
-<div>
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
 
+function ProductFilter({ products }) {
+  const context = useContext(ProductContext);
+  // eslint-disable-next-line
+  const { handleChange, type, price, minPrice, maxPrice } = context;
 
-</div>
+  //get all unique types
+  let types = getUnique(products, "type");
 
-);
+  //add all unique types
+  types = ["all", ...types];
 
+  //map items
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+  return (
+    <section className="filter-container">
+      <Title title="products showcase" />
+      <form className="filter-form">
+        {/*select product type*/}
+        <div className="form-group">
+          <label htmlFor="type">product type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/*end select product type*/}
+        <div className="form-group">
+          <label htmlFor="price">product price tshs {price}</label>
+          <input
+            type="range"
+            name="price"
+            min={minPrice}
+            max={maxPrice}
+            id="price"
+            value={price}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default ProductFilter;
-
