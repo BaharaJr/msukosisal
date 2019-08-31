2c1506ce5a13ae101ae1b9bb004b6c8029cb0413
BaharaJr <bennybenester@gmail.com>
2019-08-31 22:45:39 +0300

    :construction: Add design to the footer component see #6

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React from "react";
 
-function Footer () {
-    return (
-        <div>
-            
+function Footer() {
+  return (
+    <div>
+      <footer id="footer">
+        <div className="inner">
+          <h2>Get In Touch</h2>
+          <ul class="actions">
+            <li>
+              <span className="icon fa-phone"></span>
+              <a href="tel:+255711770224">
+                +255711770224
+              </a>
+            </li>
+            <li>
+              <span className="icon fa-envelope"></span>{" "}
+              <a href="mailto:info@msukosisal.co.tz">
+                info@msukosisal.co.tz
+              </a>
+            </li>
+            <li>
+              <span className="icon fa-map-marker"></span> Kimamba, Kilosa
+              Morogor, Tanzania
+            </li>
+            <li>
+              <span className="icon fa-facebook socials"></span>
+              <span className="icon fa-instagram socials"></span>
+              <span className="icon fa-twitter socials"></span>
+            </li>
+          </ul>
         </div>
-    );
+        <div className="copyright">
+          &copy; Msukosisal 2019
+        </div>
+      </footer>
+    </div>
+  );
 }
-
 
 export default Footer;
