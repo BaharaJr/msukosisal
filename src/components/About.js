cc144c9f85dd273c2ccfbe4fe5b8761d7bc30733
Bennett <bennybenester@gmail.com>
2019-09-21 14:06:57 +0300

    :fire: Change the about us section

--- src/components/About.js
+++ src/components/About.js
@@ -11,17 +11,12 @@ export default function About() {
       <div className="about-div">
         <Title title="Msuko Sisal" />
         <p className="about">
-          In a nutshell, Msuko Sisal is a Women Organisation based in the village
-          of Kimamba, Morogoro Tanzania. Artisans hand making interior decor
-          items from Sisal Fibre.
+          Msuko Sisal is a Women Organisation in a small village in the outskirts of morogoro, Tanzania. Within the two years since the organisation was founded, six more women had been included in the Msuko sisal family.
         </p>
       </div>
       <div className="about-div">
         <p className="about">
-          All the products are best made and sort to give you the best look on
-          natural sisal fibre products that your beauty and house deserve. With
-          all these we aim at preserving the environment by using environmental
-          friendly products.
+The Purpose of Msuko Sisal is to uplift and provide income for the women in the village, all the same time to create a growing environment that supports and encourages creativity. With the minimum guidelines of the organisation to create a sisal home decor that are importantly eco-friendly.
         </p>
       </div>
     </>
